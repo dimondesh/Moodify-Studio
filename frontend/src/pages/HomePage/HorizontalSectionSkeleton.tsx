7c24f2e3fb4335bdbc1b6e2d1d60bcffebaabb39
dimondesh <dimondesh1337@gmail.com>
2025-09-03 11:30:17 +0300

    fix

--- frontend/src/pages/HomePage/HorizontalSectionSkeleton.tsx
+++ frontend/src/pages/HomePage/HorizontalSectionSkeleton.tsx
@@ -1,21 +1,28 @@
 // src/pages/HomePage/HorizontalSectionSkeleton.tsx
 
+import { ScrollArea, ScrollBar } from "../../components/ui/scroll-area";
+
 const HorizontalSectionSkeleton = () => {
   return (
     <div className="mb-8">
       <div className="h-8 w-48 bg-zinc-800 rounded mb-4 animate-pulse" />
-      <div className="flex space-x-4">
-        {Array.from({ length: 5 }).map((_, i) => (
-          <div
-            key={i}
-            className="bg-zinc-800/40 p-4 rounded-md animate-pulse w-40 sm:w-48 flex-shrink-0"
-          >
-            <div className="aspect-square rounded-md bg-zinc-700 mb-4" />
-            <div className="h-4 bg-zinc-700 rounded w-3/4 mb-2" />
-            <div className="h-4 bg-zinc-700 rounded w-1/2" />
-          </div>
-        ))}
-      </div>
+
+      <ScrollArea className="w-full whitespace-nowrap rounded-md">
+        <div className="flex space-x-4 pb-4">
+          {" "}
+          {Array.from({ length: 5 }).map((_, i) => (
+            <div
+              key={i}
+              className="bg-zinc-800/40 p-4 rounded-md animate-pulse w-40 sm:w-48 flex-shrink-0"
+            >
+              <div className="aspect-square rounded-md bg-zinc-700 mb-4" />
+              <div className="h-4 bg-zinc-700 rounded w-3/4 mb-2" />
+              <div className="h-4 bg-zinc-700 rounded w-1/2" />
+            </div>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   );
 };
